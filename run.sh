4292d7fab2a381929bd24719b141227c3c1341cd
Marc Schweikert <schweikertm@udev.local>
2018-10-09 15:36:04 -0400

    Fix run command

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ if [[ ! -d ${PERSIST} ]]; then
     exit 1
 fi
 
-docker run -it -p 5901:5901 -v ${PERSIST}:${TARGET} android-studio:latest
+docker run -p 5901:5901 -v ${PERSIST}:${TARGET} android-studio:latest /dockerstartup/vnc_startup.sh --wait
